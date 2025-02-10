825b1bfa0f65715b9ae8a8bc2dfaeb4499d7473a
jresmer <jpresmer@gmail.com>
2025-02-10 00:33:08 -0300

    parseMessage updated to check for formating correctness

--- webpage/src/main.js
+++ webpage/src/main.js
@@ -14,6 +14,7 @@ JSON template:
 // JSON message parsing function
 function parseMessage (message) {
     const obj = JSON.parse(message);
+    const correct_keys = ["temperature", "voltage", "average_voltage", "current", "speed0", "latitude", "longitude", "year", "month", "day", "hour", "minute", "second"];
     obj.temperature = parseFloat(obj.temperature);
     obj.voltage = parseFloat(obj.voltage);
     obj.average_voltage = parseFloat(obj.average_voltage);
@@ -25,17 +26,11 @@ function parseMessage (message) {
     [obj.hour, obj.minute, obj.second] = obj.datetime.slice(12, -1).split(":");
     delete obj.datetime;
 
+    // checks for correctness of message formating
+    for (const [key, value] of Object.entries(obj)) {
+        if (!key in correct_keys) return;
+        else if (isNaN(value)) return;
+    }
+
     return obj;
 }
-
-console.log(parseMessage(`
-{
-  "temperature": "10.9",
-  "voltage": "10.9",
-  "average_voltage": "10.9",
-  "current": "10.9",
-  "speed": "10.9",
-  "latitude": "10.9",
-  "longitude": "10.9",
-  "datetime": "2009-06-15T13:45:30"
-}`))
