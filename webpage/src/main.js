968779b8d5a111297e6adbfeb0aa11824f73fe7f
jresmer <jpresmer@gmail.com>
2025-02-10 18:22:59 -0300

    parsing function updated

--- webpage/src/main.js
+++ webpage/src/main.js
@@ -1,5 +1,5 @@
 /*
-JSON template:
+template JSON:
 {
   "temperature": "10.9",
   "voltage": "10.9",
@@ -11,10 +11,17 @@ JSON template:
   "datetime": "2009-06-15T13:45:30"
 }
 */
-// JSON message parsing function
+// função de parsing de mensagens: confere corretude da mensagem
 function parseMessage (message) {
     const obj = JSON.parse(message);
     const correct_keys = ["temperature", "voltage", "average_voltage", "current", "speed0", "latitude", "longitude", "year", "month", "day", "hour", "minute", "second"];
+    // separa datetime em ano, mês, dia, hora, minuto, segundo
+    [obj.year, obj.month, obj.day] = obj.datetime.slice(0, 10).split("-");
+    [obj.hour, obj.minute, obj.second] = obj.datetime.slice(12, -1).split(":");
+    // deleta a propriedade datetime
+    delete obj.datetime;
+    // converte os valores de string para o valor numérico respectivo
+    // é necessário para garantir que o string é compatível com o tipo esperado de dado
     obj.temperature = parseFloat(obj.temperature);
     obj.voltage = parseFloat(obj.voltage);
     obj.average_voltage = parseFloat(obj.average_voltage);
@@ -22,9 +29,12 @@ function parseMessage (message) {
     obj.speed = parseFloat(obj.speed);
     obj.latitude = parseFloat(obj.latitude);
     obj.longitude = parseFloat(obj.longitude);
-    [obj.year, obj.month, obj.day] = obj.datetime.slice(0, 10).split("-");
-    [obj.hour, obj.minute, obj.second] = obj.datetime.slice(12, -1).split(":");
-    delete obj.datetime;
+    obj.year = parseInt(obj.year);
+    obj.month = parseInt(obj.month);
+    obj.day = parseInt(obj.day);
+    obj.hour = parseInt(obj.hour);
+    obj.minute = parseInt(obj.minute);
+    obj.second = parseInt(obj.second);
 
     // checks for correctness of message formating
     for (const [key, value] of Object.entries(obj)) {
@@ -32,5 +42,5 @@ function parseMessage (message) {
         else if (isNaN(value)) return;
     }
 
-    return obj;
+    return JSON.stringify(obj);
 }
